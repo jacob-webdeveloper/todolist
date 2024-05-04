5be104ca04323d5641dda3717deb3961f5d3151c
jacob-webdeveloper <kochergenjacob@gmail.com>
2024-05-04 15:43:58 +0930

    Implement remove todo function

--- todo_functions.py
+++ todo_functions.py
@@ -6,11 +6,28 @@ def add_todo(file_name):
         writer = csv.writer(f)
         writer.writerow([todo_name, "False"])
 
-def remove_todo():
-    print("Remove todo")
+def remove_todo(file_name):
+    todo_name = input("Enter the todo name that you want to remove: ")
+    # Create a new list
+    todo_list = []
+    # Put all the previous items into the list except the one they want to delete.
+    with open(file_name, "r") as f:
+        reader = csv.reader(f)
+        is_exist = False
+        for row in reader:
+            if (todo_name != row[0]):
+                todo_list.append(row)
+            else:
+                is_exist = True
+    if not is_exist:
+        print("No item with that name exists")
+    # Write the entire list.csv file with this list
+    with open(file_name, "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(todo_list)
 
 def mark_todo():
-    print("View todo")
+    print("mark todo")
 
 def view_todo(file_name):
     try:
